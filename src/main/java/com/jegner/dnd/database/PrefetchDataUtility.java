a61f0fa8a4dd278d45ac0ba47bacd03e4aaf6e65
Jo Egner <vechema@gmail.com>
2019-02-10 15:46:27 -0600

    Generate all skills and initialize in one centralized location

--- src/main/java/com/jegner/dnd/database/PrefetchDataUtility.java
+++ src/main/java/com/jegner/dnd/database/PrefetchDataUtility.java
@@ -1,26 +1,38 @@
 package com.jegner.dnd.database;
 
-import java.util.List;
+import java.io.File;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import com.jegner.dnd.database.predefined.PredefinedMaker;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jegner.dnd.database.repo.SkillRepository;
+import com.jegner.dnd.model.predefined.Skill;
 
 @Component
 public class PrefetchDataUtility {
 
 	@Autowired
-	List<PredefinedMaker<?>> predefineds;
+	SkillRepository skillRepo;
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+	private static final String PREDEFINED_JSON_PATH = "src\\main\\resources\\database\\";
 
 	public void initializeAllData() throws Exception {
 		// TODO consider some sort of self registration, common interface, and iterate
 		// through. Will have to consider order due to dependencies
+
 		// Depends on no one - Skill, Feat, Feature, Trait, Armor, Container, Item,
 		// WeaponProperty
-		for (PredefinedMaker<?> predefined : predefineds) {
-			predefined.generatePredefineds();
-		}
+		generatePredefineds(new File(PREDEFINED_JSON_PATH + "skill.json"), Skill[].class, skillRepo);
 
+	}
+
+	private <T> void generatePredefineds(File file, Class<T[]> class1, JpaRepository<T, Long> repo) throws Exception {
+		T[] predefineds = mapper.readValue(file, class1);
+		repo.saveAll(Arrays.asList(predefineds));
 	}
 }
--- src/main/java/com/jegner/dnd/database/predefined/PredefinedMaker.java
+++
@@ -1,7 +0,0 @@
-package com.jegner.dnd.database.predefined;
-
-import java.util.List;
-
-public interface PredefinedMaker<T> {
-	public List<T> generatePredefineds() throws Exception;
-}
