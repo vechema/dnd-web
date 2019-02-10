8cfb7c92968c72ab449c73f13e4aaf1b646be945
Jo Egner <vechema@gmail.com>
2019-02-10 13:10:35 -0600

    Read skills from json list into database

--- src/main/java/com/jegner/dnd/database/predefined/SkillPredefined.java
+++ src/main/java/com/jegner/dnd/database/predefined/SkillPredefined.java
@@ -1,20 +1,16 @@
 package com.jegner.dnd.database.predefined;
 
 import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jegner.dnd.database.repo.SkillRepository;
-import com.jegner.dnd.model.AbilityScore;
 import com.jegner.dnd.model.predefined.Skill;
-import com.jegner.dnd.utility.GameEntity;
 
 @Component
 public class SkillPredefined implements PredefinedMaker<Skill> {
@@ -27,25 +23,13 @@ public class SkillPredefined implements PredefinedMaker<Skill> {
 
 		List<Skill> skills = new ArrayList<>();
 
-		Skill acrobatics = new Skill();
-		// Game Entity
-		GameEntity acrobaticsGE = new GameEntity();
-		acrobaticsGE.setName("Acrobatics");
-		acrobaticsGE.setDescription(
-				"Acrobatics is a skill used when trying to maintain balance or general nimbleness. Examples include walking across a thin or rickety bridge, running along a slippery surface, or moving out of the way of a projectile. It can also be used to break free of a grapple.");
-		acrobaticsGE.setReferenceURL(new URL("https://dnd5e.fandom.com/wiki/Skills#Acrobatics"));
-		acrobatics.setGameEntity(acrobaticsGE);
-		// Ability Score/modifier
-		acrobatics.setAbilityModifier(AbilityScore.DEXTERITY);
-		skills.add(acrobatics);
-
 		ObjectMapper mapper = new ObjectMapper();
 		File skillFile = new File("src\\main\\resources\\database\\skill.json");
-		Skill arcana = mapper.readValue(skillFile, Skill.class);
-		skills.add(arcana);
+		Skill[] arcana = mapper.readValue(skillFile, Skill[].class);
+		skills.addAll(Arrays.asList(arcana));
 
 		skillRepo.saveAll(skills);
-		return Arrays.asList(acrobatics);
+		return skills;
 	}
 
 }
